43103cdf20bc67d28b417d059749b7cbbf668a9a
Benjamin Sommerfeld <bsommerfeld@users.noreply.github.com>
2025-06-16 02:55:33 +0200

    refactor: remove deprecated PathEnvironment methods and cleanup unused imports in BukkitMapper

--- core/src/main/java/de/bsommerfeld/pathetic/bukkit/mapper/BukkitMapper.java
+++ core/src/main/java/de/bsommerfeld/pathetic/bukkit/mapper/BukkitMapper.java
@@ -1,59 +1,15 @@
 package de.bsommerfeld.pathetic.bukkit.mapper;
 
-import java.util.Arrays;
-
+import de.bsommerfeld.pathetic.api.wrapper.PathPosition;
+import de.bsommerfeld.pathetic.api.wrapper.PathVector;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.util.Vector;
-import de.bsommerfeld.pathetic.api.wrapper.PathEnvironment;
-import de.bsommerfeld.pathetic.api.wrapper.PathPosition;
-import de.bsommerfeld.pathetic.api.wrapper.PathVector;
-import de.bsommerfeld.pathetic.engine.util.ErrorLogger;
 
 @UtilityClass
 public class BukkitMapper {
-
-  private final boolean IS_NEWER_WORLD;
-
-  static {
-    IS_NEWER_WORLD =
-            Arrays.stream(World.class.getMethods())
-                    .anyMatch(method -> "getMinHeight".equalsIgnoreCase(method.getName()));
-  }
-
-  /**
-   * @deprecated PathEnvironment is deprecated. Use {@link #toLocation(PathPosition, World)} instead.
-   */
-  @Deprecated
-  @NonNull
-  public Location toLocation(@NonNull PathPosition pathPosition) {
-    return new Location(
-            toWorld(pathPosition.getPathEnvironment()),
-            pathPosition.getX(),
-            pathPosition.getY(),
-            pathPosition.getZ());
-  }
-
-  /**
-   * @deprecated PathEnvironment is deprecated. Use World directly instead.
-   */
-  @Deprecated
-  public World toWorld(@NonNull PathEnvironment pathEnvironment) {
-    return Bukkit.getWorld(pathEnvironment.getUuid());
-  }
-
-  /**
-   * @deprecated PathEnvironment is deprecated. Use World directly instead.
-   */
-  @Deprecated
-  @NonNull
-  public PathEnvironment toPathWorld(@NonNull World world) {
-    return new PathEnvironment(
-            world.getUID(), world.getName(), getMinHeight(world), getMaxHeight(world));
-  }
 
   /**
    * Converts a PathPosition to a Bukkit Location using the provided World.
@@ -64,11 +20,7 @@ public class BukkitMapper {
    */
   @NonNull
   public Location toLocation(@NonNull PathPosition pathPosition, @NonNull World world) {
-    return new Location(
-            world,
-            pathPosition.getX(),
-            pathPosition.getY(),
-            pathPosition.getZ());
+    return new Location(world, pathPosition.getX(), pathPosition.getY(), pathPosition.getZ());
   }
 
   /**
@@ -102,13 +54,5 @@ public class BukkitMapper {
   @NonNull
   public PathVector toPathVector(@NonNull Vector vector) {
     return new PathVector(vector.getX(), vector.getY(), vector.getZ());
-  }
-
-  private int getMinHeight(@NonNull World world) {
-    return IS_NEWER_WORLD ? world.getMinHeight() : 0;
-  }
-
-  private int getMaxHeight(@NonNull World world) {
-    return IS_NEWER_WORLD ? world.getMaxHeight() : 256;
   }
 }
